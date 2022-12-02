c0a57524af7c5bd82c1ccb644195ca1472ecade2
jorektheglitch <lozcontrariuz@yandex.ru>
2022-12-02 22:52:27 +0300

    Specify JWT's stucture

--- microchat/core/jwt_manager.py
+++ microchat/core/jwt_manager.py
@@ -1,6 +1,16 @@
-from typing import Any
+from typing import TypedDict
+from typing import cast
 
 import jwt
+
+
+class SessionInfo(TypedDict):
+    session: int
+    user: int
+
+
+class IncompleteJWT(Exception):
+    pass
 
 
 class JWTManager:
@@ -9,8 +19,12 @@ class JWTManager:
     def __init__(self, secret: str) -> None:
         self.secret = secret
 
-    def decode(self, token: str) -> dict[str, Any]:
-        return jwt.decode(token, self.secret)
+    def decode_access_token(self, token: str) -> SessionInfo:
+        return cast(SessionInfo, jwt.decode(token, self.secret))
 
-    def encode(self, payload: dict[str, Any]) -> str:
-        return jwt.encode(payload, self.secret)
+    def decode_csrf_token(self, token: str) -> SessionInfo:
+        return cast(SessionInfo, jwt.decode(token, self.secret))
+
+    def create_access_token(self, user_id: int, session_id: int) -> str:
+        session_info = dict(session=session_id, user=user_id)
+        return jwt.encode(session_info, self.secret)
