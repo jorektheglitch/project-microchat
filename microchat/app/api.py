57f4678689b03d20b0496c47c1c1719c6d30d442
jorektheglitch <lozcontrariuz@yandex.ru>
2022-12-07 01:08:30 +0300

    Move all router things to separated module

--- microchat/app/api.py
+++ microchat/app/api.py
@@ -1,75 +1,17 @@
-from typing import Awaitable, Callable, Sequence, TypeVar
+from typing import Callable
 
 from aiohttp import web
-from microchat.api_utils.handler import api_handler
-from microchat.api_utils.request import APIRequest
-from microchat.api_utils.response import APIResponse
 
 from microchat.core.jwt_manager import JWTManager
 from microchat.storages import UoW
-from microchat.services import ServiceSet
-from microchat.api_utils.types import Handler, Middleware
 
-from microchat.api.auth_ import add_session, list_sessions, terminate_session
-from microchat.api.chats import router as chats_routes
-from microchat.api.entities import router as entities_routes
-from microchat.api.media import router as media_routes
-
-from .api_adapters.auth import session_add_params, session_close_params, sessions_request_params
-
-
-R = TypeVar("R", bound=APIRequest, contravariant=True)
-
-APIRoute = tuple[str, str, Callable[[R, ServiceSet], Awaitable[APIResponse]], Callable[[web.Request], Awaitable[R]]]
-auth_routes = web.UrlDispatcher()
-auth_routes.add_route("GET", "/sessions", api_handler(list_sessions))
-auth_routes_: list[APIRoute[APIRequest]] = [
-    ("GET", "/sessions", list_sessions, sessions_request_params, authenticate),
-    ("POST", "/sessions", add_session, session_add_params),
-    (r"/sessions/{session_id:\w+}", terminate_session, session_close_params)
-]
-
-
-SUBROUTES: dict[str, Sequence[web.AbstractRouteDef]] = {
-    "/auth/": auth_routes,
-    "/chats/": chats_routes,
-    "/entities/": entities_routes,
-    "/media/": media_routes
-}
-
-
-def jwt_middleware(jwt_manager: JWTManager) -> Middleware:
-    @web.middleware
-    async def add_jwt_manager(
-        request: web.Request, handler: Handler
-    ) -> web.StreamResponse:
-        request["jwt_manager"] = jwt_manager
-        return await handler(request)
-    return add_jwt_manager
-
-
-def services_middleware(uow_factory: Callable[[], UoW]) -> Middleware:
-    @web.middleware
-    async def add_services(
-        request: web.Request, handler: Handler
-    ) -> web.StreamResponse:
-        async with uow_factory() as uow:
-            services = ServiceSet(uow)
-            request["services"] = services
-            return await handler(request)
-    return add_services
+from .routes import get_api_router
 
 
 def api_app(
     uow_factory: Callable[[], UoW],
     jwt_manager: JWTManager
 ) -> web.Application:
-    api_app = web.Application(middlewares=[
-        jwt_middleware(jwt_manager),
-        services_middleware(uow_factory)
-    ])
-    for prefix, routes in SUBROUTES.items():
-        subapp = web.Application()
-        subapp.router.add_routes(routes)
-        api_app.add_subapp(prefix, subapp)
+    router = get_api_router(uow_factory, jwt_manager)
+    api_app = web.Application(router=router)
     return api_app
---
+++ microchat/app/routes.py
@@ -0,0 +1,73 @@
+from aiohttp import web
+from aiohttp import typedefs
+
+from typing import Awaitable, Callable, TypeVar
+
+from microchat.api_utils.exceptions import APIError
+from microchat.api_utils.handler import inject_services
+from microchat.api_utils.request import APIRequest
+from microchat.api_utils.response import APIResponse, DEFAULT_JSON_DUMPER, P
+
+from microchat.core.jwt_manager import JWTManager
+from microchat.services import ServiceError, ServiceSet
+from microchat.storages import UoW
+
+from .rendering import renderer
+
+
+R = TypeVar("R", bound=APIRequest, contravariant=True)
+
+
+class APIEndpoints:
+
+    def __init__(
+        self,
+        uow_factory: Callable[[], UoW],
+        jwt_manager: JWTManager,
+        renderer: Callable[[APIResponse[P] | APIError], Awaitable[web.StreamResponse]]
+    ) -> None:
+        self.uow_factory = uow_factory
+        self.jwt_manager = jwt_manager
+        self.renderer = renderer
+        self._router = web.UrlDispatcher()
+
+    def add_route(
+        self,
+        method: str,
+        route: str,
+        executor: Callable[[R, ServiceSet], Awaitable[APIResponse[P]]],
+        extractor: Callable[[web.Request], Awaitable[R]]
+    ) -> None:
+        with_services = inject_services(executor, self.uow_factory, self.jwt_manager)
+        handler = endpoint(with_services, extractor, self.renderer)
+        self._router.add_route(method, route, handler)
+
+
+def get_api_router(
+    uow_factory: Callable[[], UoW],
+    jwt_manager: JWTManager
+) -> web.UrlDispatcher:
+    render = renderer(DEFAULT_JSON_DUMPER)
+    routes = APIEndpoints(uow_factory, jwt_manager, render)
+    # TODO: add all routes 
+    return routes._router
+
+
+def endpoint(
+    executor: Callable[[R], Awaitable[APIResponse[P]]],
+    extractor: Callable[[web.Request], Awaitable[R]],
+    renderer: Callable[[APIResponse[P] | APIError], Awaitable[web.StreamResponse]],
+) -> typedefs.Handler:
+    async def handler(request: web.Request) -> web.StreamResponse:
+        api_response: APIResponse[P] | APIError
+        try:
+            api_request = await extractor(request)
+            api_response = await executor(api_request)
+        except APIError as err:
+            api_response = err
+        except ServiceError as service_exc:
+            exc_info = APIError.from_service_exc(service_exc)
+            api_response = exc_info
+        reponse = await renderer(api_response)
+        return reponse
+    return handler
