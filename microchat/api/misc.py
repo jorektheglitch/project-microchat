60a2692ffa6955d37e331b3f9f0ddd4a55eb7a6d
jorektheglitch <lozcontrariuz@yandex.ru>
2022-12-12 01:26:12 +0300

    Remove old useless authenticator

--- microchat/api/misc.py
+++ microchat/api/misc.py
@@ -2,11 +2,6 @@ from dataclasses import dataclass
 
 from typing import TypedDict
 
-from aiohttp import web
-
-from microchat.core.entities import User
-from microchat.services import ServiceSet, AuthenticationError
-
 
 @dataclass
 class Disposition:
@@ -23,18 +18,3 @@ class PermissionsPatch(TypedDict):
     add_user: bool | None
     pin_message: bool | None
     edit_conference: bool | None
-
-
-async def authenticate_media_request(
-    request: web.Request, services: ServiceSet
-) -> User:
-    try:
-        auth_cookie = request.cookies["MEDIA_ACCESS"]
-        csrf_token = request.query["csrf_token"]
-    except KeyError:
-        raise AuthenticationError
-    session = await services.auth.resolve_media_token(
-        auth_cookie, csrf_token
-    )
-    user = session.auth.user
-    return user
