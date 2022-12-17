71403fd4f0c01f2c6f9bb0404dd66815b23261db
jorektheglitch <lozcontrariuz@yandex.ru>
2022-12-17 22:34:38 +0300

    Fix Chats API related extractors

--- microchat/app/api_adapters/chats.py
+++ microchat/app/api_adapters/chats.py
@@ -1,11 +1,16 @@
-from dataclasses import dataclass
-
 from aiohttp import web
+
+from microchat.api.chats import GetMessage, GetChat, GetChatMedia
+from microchat.api.chats import GetAttachmentContent, GetAttachmentPreview
+from microchat.api.chats import GetChatMedias, GetChats, GetMessages
+from microchat.api.chats import EditMessage, DeleteMessage
+from microchat.api.chats import RemoveChatMedia
+from microchat.api.chats import SendMessage
 from microchat.api_utils.exceptions import BadRequest, NotFound
 
-from microchat.api_utils.request import APIRequest
 from microchat.core.entities import Animation, Audio, File, Image, Video
-from .misc import Disposition, get_disposition, get_request_payload, int_param
+from .misc import get_disposition, get_request_payload, int_param
+from .misc import get_access_token, get_media_access_info
 
 
 MEDIA_CLASSES: dict[str, type[Image | Animation | Audio | Video | File]] = {
@@ -17,110 +22,42 @@ MEDIA_CLASSES: dict[str, type[Image | Animation | Audio | Video | File]] = {
 }
 
 
-@dataclass
-class ChatsAPIRequest(APIRequest):
-    pass
-
-
-@dataclass
-class GetChats(ChatsAPIRequest):
-    disposition: Disposition
-
-
-@dataclass
-class GetChat(ChatsAPIRequest):
-    chat: str | int
-
-
-@dataclass
-class GetMessages(ChatsAPIRequest):
-    chat_request: GetChat
-    disposition: Disposition
-
-
-@dataclass
-class GetMessage(ChatsAPIRequest):
-    chat_request: GetChat
-    message_id: int
-
-
-@dataclass
-class SendMessage(ChatsAPIRequest):
-    chat_request: GetChat
-    text: str | None
-    attachments: list[int] | None
-    reply_to: int | None
-
-
-@dataclass
-class EditMessage(ChatsAPIRequest):
-    message_request: GetMessage
-    text: str | None
-    attachments: list[int] | None
-
-
-@dataclass
-class DeleteMessage(ChatsAPIRequest):
-    message_request: GetMessage
-
-
-@dataclass
-class GetAttachmentPreview(ChatsAPIRequest):
-    message_request: GetMessage
-    attachment_id: int
-
-
-@dataclass
-class GetAttachmentContent(ChatsAPIRequest):
-    message_request: GetMessage
-    attachment_id: int
-
-
-@dataclass
-class GetChatMedias(ChatsAPIRequest):
-    chat_requset: GetChat
-    disposition: Disposition
-    media_type: type[Image | Animation | Audio | Video | File]
-
-
-@dataclass
-class ChatMediaRequest(ChatsAPIRequest):
-    chat_request: GetChat
-    media_id: int
-    media_type: type[Image | Animation | Audio | Video | File]
-
-
 async def chats_request_params(request: web.Request) -> GetChats:
+    access_token = get_access_token(request)
     disposition = get_disposition(request)
-    return GetChats(disposition)
+    return GetChats(access_token, disposition)
 
 
 async def chat_request_params(request: web.Request) -> GetChat:
+    access_token = get_access_token(request)
     entity_id = request.match_info.get("entity_id")
     alias = request.match_info.get("alias")
     if entity_id:
         chat = int_param(entity_id)
-        return GetChat(chat)
+        return GetChat(access_token, chat)
     elif alias:
-        return GetChat(alias)
+        return GetChat(access_token, alias)
     else:
         raise BadRequest
 
 
 async def messages_request_params(request: web.Request) -> GetMessages:
+    access_token = get_access_token(request)
     chat_request = await chat_request_params(request)
     disposition = get_disposition(request)
-    return GetMessages(chat_request, disposition)
+    return GetMessages(access_token, chat_request, disposition)
 
 
 async def message_request_params(request: web.Request) -> GetMessage:
+    access_token = get_access_token(request)
     chat_request = await chat_request_params(request)
     message_id_repr = request.match_info.get("message_id", "")
     message_id = int_param(message_id_repr)
-    return GetMessage(chat_request, message_id)
+    return GetMessage(access_token, chat_request, message_id)
 
 
 async def message_send_params(request: web.Request) -> SendMessage:
+    access_token = get_access_token(request)
     chat_request = await chat_request_params(request)
     payload = await get_request_payload(request)
     text = payload.get("text")
@@ -132,10 +69,13 @@ async def message_send_params(request: web.Request) -> SendMessage:
         raise BadRequest("Message text or attachments must be given")
     if reply_to_id is not None and not isinstance(reply_to_id, int):
         raise BadRequest("'reply_to' must be int")
-    return SendMessage(chat_request, text, attachments_ids, reply_to_id)
+    return SendMessage(
+        access_token, chat_request, text, attachments_ids, reply_to_id
+    )
 
 
 async def message_edit_params(request: web.Request) -> EditMessage:
+    access_token = get_access_token(request)
     message_request = await message_request_params(request)
     payload = await get_request_payload(request)
     text = payload.get("text")
@@ -144,43 +84,52 @@ async def message_edit_params(request: web.Request) -> EditMessage:
         raise BadRequest("Attachments must be list of ints")
     if text is None and not attachments_ids:
         raise BadRequest("Message text or attachments must be given")
-    return EditMessage(message_request, text, attachments_ids)
+    return EditMessage(access_token, message_request, text, attachments_ids)
 
 
 async def message_delete_params(request: web.Request) -> DeleteMessage:
+    access_token = get_access_token(request)
     message_request = await message_request_params(request)
-    return DeleteMessage(message_request)
+    return DeleteMessage(access_token, message_request)
 
 
 async def attachment_content_params(
     request: web.Request
 ) -> GetAttachmentContent:
+    access_token, csrf_token = get_media_access_info(request)
     message_request = await message_request_params(request)
     attachment_id_repr = request.match_info.get("attachment_id", "")
     attachment_id = int_param(attachment_id_repr)
-    return GetAttachmentContent(message_request, attachment_id)
+    return GetAttachmentContent(
+        access_token, csrf_token, message_request, attachment_id
+    )
 
 
 async def attachment_preview_params(
     request: web.Request
 ) -> GetAttachmentPreview:
+    access_token, csrf_token = get_media_access_info(request)
     message_request = await message_request_params(request)
     attachment_id_repr = request.match_info.get("attachment_id", "")
     attachment_id = int_param(attachment_id_repr)
-    return GetAttachmentPreview(message_request, attachment_id)
+    return GetAttachmentPreview(
+        access_token, csrf_token, message_request, attachment_id
+    )
 
 
 async def medias_request_params(request: web.Request) -> GetChatMedias:
+    access_token = get_access_token(request)
     chat_request = await chat_request_params(request)
     disposition = get_disposition(request)
     media_type_repr = request.match_info.get("media_type", "")
     media_type = MEDIA_CLASSES.get(media_type_repr)
     if media_type is None:
         raise NotFound(f"Unknown media type: '{media_type_repr}'")
-    return GetChatMedias(chat_request, disposition, media_type)
+    return GetChatMedias(access_token, chat_request, disposition, media_type)
 
 
-async def media_request_params(request: web.Request) -> ChatMediaRequest:
+async def media_request_params(request: web.Request) -> GetChatMedia:
+    access_token = get_access_token(request)
     chat_request = await chat_request_params(request)
     media_id_repr = request.match_info.get("media_id", "")
     media_id = int_param(media_id_repr)
@@ -188,4 +137,16 @@ async def media_request_params(request: web.Request) -> ChatMediaRequest:
     media_type = MEDIA_CLASSES.get(media_type_repr)
     if media_type is None:
         raise NotFound(f"Unknown media type: '{media_type_repr}'")
-    return ChatMediaRequest(chat_request, media_id, media_type)
+    return GetChatMedia(access_token, chat_request, media_id, media_type)
+
+
+async def media_delete_params(request: web.Request) -> RemoveChatMedia:
+    access_token = get_access_token(request)
+    chat_request = await chat_request_params(request)
+    media_id_repr = request.match_info.get("media_id", "")
+    media_id = int_param(media_id_repr)
+    media_type_repr = request.match_info.get("media_type", "")
+    media_type = MEDIA_CLASSES.get(media_type_repr)
+    if media_type is None:
+        raise NotFound(f"Unknown media type: '{media_type_repr}'")
+    return RemoveChatMedia(access_token, chat_request, media_id, media_type)
