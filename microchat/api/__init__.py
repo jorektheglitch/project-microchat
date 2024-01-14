9bdc9afb261d578bfb070e3010fca05934ce57fd
jorektheglitch <jorektheglitch@yandex.ru>
2024-01-14 15:16:37 +0300

    Add imports to `__init__.py`

---
+++ microchat/api/__init__.py
@@ -0,0 +1,8 @@
+from . import auth  # noqa: F401
+from . import chats  # noqa: F401
+from . import conferences  # noqa: F401
+from . import contacts  # noqa: F401
+from . import entities  # noqa: F401
+from . import events  # noqa: F401
+from . import media  # noqa: F401
+from . import misc  # noqa: F401
